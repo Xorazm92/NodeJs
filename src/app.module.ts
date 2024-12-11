a82d45893c1e3b7b922c86c7f4c7199002250447
Xorazm92 <rayadunia2021@gmail.com>
2024-12-11 09:54:14 +0500

    "Boshladim"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './modules/todo/todo.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(), TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
